28f4b53310101c6e1f1f7ce4bc2b17a594466b54
hoang anh <hoanganhctm7@gmail.com>
2018-10-14 15:11:35 +0700

    done 124

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/_Aux';
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.4,
+  cheese: 1.3,
+  meat: 0.7
+};
 
 class BurgerBuilder extends Component {
   state = {
@@ -9,14 +17,54 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = (type) => {
+    let newIngredients = {...this.state.ingredients};
+    newIngredients[type] += 1;
+
+    let newTotalPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+
+    this.setState({
+      ingredients: newIngredients,
+      totalPrice: newTotalPrice
+    });
+  };
+
+  removeIngredientHandler = (type) => {
+    let newIngredients = {...this.state.ingredients};
+    if (newIngredients[type] <= 0) {
+      return;
     }
+
+    newIngredients[type] -= 1;
+
+    let newTotalPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+
+    this.setState({
+      ingredients: newIngredients,
+      totalPrice: newTotalPrice
+    });
   };
 
   render() {
+    const disabledInfo = {...this.state.ingredients};
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls
+          addIngredient={this.addIngredientHandler}
+          removeIngredient={this.removeIngredientHandler}
+          disabledInfo={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </Aux>
     );
   }
